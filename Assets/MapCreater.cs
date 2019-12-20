523d01deac058f8a207f03073bc20506eed613aa
Iwancof <rock.iwancof@gmail.com>
2019-12-20 16:22:24 +0900

    初期コミット

---
+++ Assets/MapCreater.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map {
+    MapChip[][] MapData;
+    //あえてジャグ配列
+
+    public int Height { get; private set; }
+    public int Width { get; private set; }
+
+    public Map(int height,int width) {
+        Height = height;
+        Width = width;
+
+        MapData = new MapChip[Width][];
+        for(int x = 0;x < Width;x++) {
+            MapData[x] = new MapChip[Height];
+            for(int y = 0;y < Height;y++) {
+                MapData[x][y] = MapChip.None;
+            }
+        }
+        MapData[2][3] = MapChip.Wall;
+    }
+}
+
+enum MapChip { 
+    None = 0,
+    Wall = 1,
+    Respown = 2,
+    //etc...
+}
+
+public static class MapCreater
+{
+    public static Map CreateTestMap() {
+        return new Map(10, 10);
+    }
+}
---
+++ Assets/MapDrawer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapShader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Map map;
+
+    void Start()
+    {
+        map = MapCreater.CreateTestMap();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int x = 0;x < map.Width;x++) {
+            for(int y = 0;y < map.Height;y++) {
+
+            }
+        }
+    }
+}
