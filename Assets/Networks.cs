96dfb9b366cc4aeacbc72564fd6dae54e0f0e7b3
Iwancof <rock.iwancof@gmail.com>
2019-12-20 17:38:10 +0900

    サーバ関連の追加、TcpListenerのプロトタイプ、Unity側のClientの追加予定

---
+++ Assets/Networks.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+public class NetworksManager
+{
+    TcpClient client = new TcpClient("localhost", 1000);
+
+    void 
+}
