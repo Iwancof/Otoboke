dd2a8bf7e9e33ffac265138b672655c79a259282
wakuto <waku.902002@gmail.com>
2020-02-10 16:58:54 +0900

    プレイヤーの状況確認の修正

--- Assets/GameScripts/Player.cs
+++ Assets/GameScripts/Player.cs
@@ -1,11 +1,11 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
-    private int x = 0;
-    private int y = 0;
+public class Player : MonoBehaviour {
+    private float x = 0;
+    private float y = 0;
     public float speed = 1.0f;
 
     private enum direction {
@@ -15,6 +15,7 @@ public class Player : MonoBehaviour
         left = 3
     }
     direction state;
+    direction buffer;
 
     private Dictionary<direction, Ray> ray = new Dictionary<direction, Ray>();
     private RaycastHit hit;
@@ -22,78 +23,51 @@ public class Player : MonoBehaviour
     private float mar = 0.005f;
 
     private Rigidbody2D rb;
-    
 
-    void Start()
-    {
+
+    void Start() {
         rb = GetComponent<Rigidbody2D>();
         rad = GetComponent<CircleCollider2D>().radius;
     }
 
-    void Update()
-    {
+    void Update() {
         InputProcessing();
-        Debug.DrawRay(ray[direction.up].origin, new Vector2(0, rad), Color.red);
-        Debug.DrawRay(ray[direction.down].origin, new Vector2(0, -rad), Color.red);
-        Debug.DrawRay(ray[direction.left].origin, new Vector2(-rad, 0), Color.red);
-        Debug.DrawRay(ray[direction.right].origin, new Vector2(rad, 0), Color.red);
+        CheckStatus();
+        Move();
 
-        rb.velocity = new Vector2(x, y) * speed;
     }
 
     void InputProcessing() {
         if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) {
-            x = 0;
-            y = 1;
-            state = direction.up;
+            buffer = direction.up;
         } else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) {
-            x = 0;
-            y = -1;
-            state = direction.down;
+            buffer = direction.down;
+        } else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) {
+            buffer = direction.right;
+        } else if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) {
+            buffer = direction.left;
         }
-
-        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) {
-            y = 0;
-            x = 1;
-            state = direction.right;
-        } else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) {
-            y = 0;
-            x = -1;
-            state = direction.left;
-        }
-
-        ray[direction.up] = new Ray(rb.position, new Vector2(0, rad + mar));
-        ray[direction.down] = new Ray(rb.position, new Vector2(0, -rad -mar));
-        ray[direction.right] = new Ray(rb.position, new Vector2(rad + mar, 0));
-        ray[direction.left] = new Ray(rb.position, new Vector2(-rad - mar, 0));
     }
 
-    private direction[] GetSide(direction dir) {
-        var tmp = new direction[2];
-        switch (dir) {
-            case direction.up: {
-                goto case direction.down;
-            }
-            case direction.down: {
-                tmp[0] = direction.left;
-                tmp[1] = direction.right;
-                return tmp;
-            }
-            case direction.left: { 
-                tmp[0] = direction.up;
-                tmp[1] = direction.down;
-                goto case direction.right;
-            }
-            case direction.right: {
-                tmp[0] = direction.up;
-                tmp[1] = direction.down;
-                return tmp;
-            }
-            default: {
-                tmp[0] = direction.left;
-                tmp[1] = direction.right;
-                return tmp;
+    void CheckStatus() {
+        ray[direction.up] = new Ray(rb.position, new Vector2(0, rad + mar));
+        ray[direction.down] = new Ray(rb.position, new Vector2(0, -rad - mar));
+        ray[direction.right] = new Ray(rb.position, new Vector2(rad + mar, 0));
+        ray[direction.left] = new Ray(rb.position, new Vector2(-rad - mar, 0));
+
+        foreach(var dir in Enum.GetValues(typeof(direction))) { 
+            if(Physics.Raycast(ray[i], out hit, 10.0f)) {
+
             }
         }
+
+        Debug.DrawRay(ray[direction.up].origin, new Vector2(0, rad), Color.red);
+        Debug.DrawRay(ray[direction.down].origin, new Vector2(0, -rad), Color.red);
+        Debug.DrawRay(ray[direction.left].origin, new Vector2(-rad, 0), Color.red);
+        Debug.DrawRay(ray[direction.right].origin, new Vector2(rad, 0), Color.red);
+    }
+
+    void Move() {
+        rb.velocity = new Vector2(x, y) * speed;
     }
 }
