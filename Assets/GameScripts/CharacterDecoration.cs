0d78f12687b96f6f5635f50874fa1f0259f8db9f
wakuto <waku.902002@gmail.com>
2020-05-09 21:01:01 +0900

    add opening animation

---
+++ Assets/GameScripts/CharacterDecoration.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterDecoration : MonoBehaviour
+{
+    Text text;
+    enum DecorationType {
+        synchroBlink,
+        turnBlink,
+    }
+    [SerializeField]
+    Color color;
+    Color prevColor;
+    [SerializeField]
+    DecorationType decoration;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        prevColor = text.color;
+
+        switch(decoration) {
+            case DecorationType.synchroBlink: {
+                StartCoroutine(Synchro());
+                break;
+            }
+            case DecorationType.turnBlink: {
+                break;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Synchro() {
+        while(true) {
+            text.color = color;
+            yield return new WaitForSeconds(0.2f);
+            text.color = prevColor;
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+
+    IEnumerator Turn() {
+        while(true) {
+            int i = 0;
+            while(text.text.Length > i) {
+                //text.text.
+                yield return new WaitForSeconds(0.2f);
+            }
+            
+        }
+    }
+}
---
+++ Assets/OpeningController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpeningController : MonoBehaviour
+{
+    Vector3 startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-Vector3.right * 1.5f * Time.deltaTime);
+        if(transform.position.x < -12f) {
+            transform.position = startPosition;
+        }
+    }
+}
