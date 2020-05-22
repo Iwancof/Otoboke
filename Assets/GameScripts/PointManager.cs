312fe8eba5f513d9c1d38a63e69f96c67fc107fd
wakuto <waku.902002@gmail.com>
2020-05-22 18:53:25 +0900

    Add Leftovers status

--- Assets/GameScripts/PointManager.cs
+++ Assets/GameScripts/PointManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PointManager : MonoBehaviour
 {
@@ -11,12 +12,18 @@ public class PointManager : MonoBehaviour
     float duration, diff;
     public static int numofBite = 0;
     public static bool defeat = false;
+    Image img;
+    Slider slider;
+    int prevBites;
     void Start()
     {
         duration = 10;
         defeat = false;
         numofBite = 0;
         baites = -1;
+        slider = GameObject.Find("Canvas/Slider").GetComponent<Slider>();
+        img = GameObject.Find("Canvas/Slider/FillArea/Fill").GetComponent<Image>();
+        img.color = new Color(0, 255, 0);
 
     }
 
@@ -29,10 +36,20 @@ public class PointManager : MonoBehaviour
             }
         }        
         if(numofBite <= baites) { // パックマンが餌を食べ尽くした
-            //Debug.LogWarning("Defeat");
             // ゲームオーバー処理
             defeat = true;
         }
+        if (numofBite > 0) {
+            slider.value = (float)(numofBite - baites) / numofBite;
+        }
+        if(prevBites != baites) {
+            // r -> value 1～0.5 の間0から255まで上がる
+            // g -> value 0.5～0 の間255から0まで下がる
+            img.color = new Color(slider.value >= 0.5 ? (float)(1-(slider.value-0.5) * 2) : 1, slider.value < 0.5 ? (float)(slider.value * 2) : 1, 0);
+            Debug.Log(img.color);
+        }
+        prevBites = baites;
+
         
     }
 }
