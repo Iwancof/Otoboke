090695592f12f07d77e0caa009150416237d64fb
wakuto <waku.902002@gmail.com>
2020-05-23 23:27:53 +0900

    fix: teleport & refactering

--- Assets/GameScripts/PacmanController.cs
+++ Assets/GameScripts/PacmanController.cs
@@ -5,60 +5,43 @@ using System.Linq;
 
 public class PacmanController: MonoBehaviour
 {
-    Animator anim;
-    Vector3 firstPos;
-    Vector3 nowPos;
-    Vector3 prevPos;
-    Vector3 distance;
-    public Vector3 targetPos;
-
-    float startTime;
-    public float time = 10f;
-    float diff;
-
-    bool firsttime = true;
-
-    Rigidbody2D rb;
-    Dictionary<string, bool> status = new Dictionary<string, bool>();   // Playerから見た方向
-    public List<GameObject> Baits;
-
-    [SerializeField, HeaderAttribute ("うまく曲がれないときに大きくする"), Range (0, 10)]
-    public float gap = 3.0f;
-    float rad = 0f;
-    public float delta = 0.001f;
-
-    Vector3 speed = new Vector3();
-
     [SerializeField]
     AudioClip bait0, bait1;
     AudioSource source;
+    Animator anim;
+    public List<GameObject> Baits;
     bool whichSound = false; // true -> bait0 , false -> bait1
+    public float time = 10f;
+    public Vector3 targetPos;
 
+    Vector3 speed = new Vector3();
+
+    public static Vector3[] teleportPoint = new Vector3[2];
 
     void Start()
     {
         anim = GetComponent<Animator>();
-        prevPos = transform.position;
-        nowPos = transform.position;
-
-        rb = gameObject.GetComponent<Rigidbody2D>();
-        rad = gameObject.GetComponent<CircleCollider2D>().radius;
-
         source = GetComponent<AudioSource>();
-        status.Add(Vector2.up.ToString(), false);
-        status.Add(Vector2.right.ToString(), false);
-        status.Add(Vector2.down.ToString(), false);
-        status.Add(Vector2.left.ToString(), false);
-        status.Add(Vector2.zero.ToString(), false);
-
     }
 
+    bool teleportFlag = false;
     void Update()
     {
-        //nowPos = transform.position;
-        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref speed, time);
+        if(targetPos != teleportPoint[0] && targetPos != teleportPoint[1]) {
+            transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref speed, time);
+            teleportFlag = true;
+        } else if(teleportFlag) {
+            transform.position = new MySwitch<Vector3, Vector3>(targetPos)
+                                 .Case(teleportPoint[0], teleportPoint[1] - new Vector3(1f, 0, 0) * Mathf.Sign(teleportPoint[1].x))
+                                 .Case(teleportPoint[1], teleportPoint[0] - new Vector3(1f, 0, 0) * Mathf.Sign(teleportPoint[0].x))
+                                 .Default(transform.position);
+            teleportFlag = false;
+        }
 
-        //if (prevPos.x < nowPos.x) {
+        SetAnimation();
+    }
+
+    void SetAnimation() {
         if (Mathf.Abs(speed.x) > Mathf.Abs(speed.y)) {
             if (speed.x > 0) {
                 anim.speed = 1.0f;
@@ -66,14 +49,12 @@ public class PacmanController: MonoBehaviour
                 anim.SetBool ("left", false);
                 anim.SetBool ("up", false);
                 anim.SetBool ("down", false);
-            //} else if (prevPos.x > nowPos.x) {
             } else if (speed.x < 0) {
                 anim.speed = 1.0f;
                 anim.SetBool ("right", false);
                 anim.SetBool ("left", true);
                 anim.SetBool ("up", false);
                 anim.SetBool ("down", false);
-            //} else if (prevPos.y < nowPos.y) {
             } 
         } else if (Mathf.Abs(speed.y) > Mathf.Abs(speed.x)) {
             if (speed.y > 0) {
@@ -82,7 +63,6 @@ public class PacmanController: MonoBehaviour
                 anim.SetBool ("left", false);
                 anim.SetBool ("up", true);
                 anim.SetBool ("down", false);
-            //} else if (prevPos.y > nowPos.y) {
             } else if (speed.y < 0) {
                 anim.speed = 1.0f;
                 anim.SetBool ("right", false);
@@ -93,7 +73,6 @@ public class PacmanController: MonoBehaviour
         } else {
             anim.speed = 0;
         }
-        prevPos = transform.position;
     }
 
     public void PacBaitAt(int x, int y) {
@@ -118,30 +97,6 @@ public class PacmanController: MonoBehaviour
                 Destroy(paced_object.Item2);
                 break;
         }
-
-
-        /*
-        switch (MapController.map.MapData[x][y]) {
-            case MapChip.Bait:
-                if (PointManager.baites == -1) PointManager.baites = 0;
-                PointManager.baites++;
-                whichSound = !whichSound;
-                if (whichSound)
-                    source.clip = bait0;
-                else
-                    source.clip = bait1;
-                source.Play();
-                Destroy(FindBaitObjectByCoordinate(x, y));
-                break;
-            case MapChip.PowerBait:
-                if (PointManager.baites == -1) PointManager.baites = 0;
-                PointManager.baites++;
-                PointManager.startTime = Time.timeSinceLevelLoad;
-                PointManager.hasPower = true;
-                Destroy(FindBaitObjectByCoordinate(x, y));
-                break;
-        }
-        */
     }
 
     public (MapChip, GameObject) FindBaitObjectByCoordinate(int px, int py) {
