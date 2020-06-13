cda3ed4742cbf1ae1d4b0351968bcf54c9218241
wakuto <waku.902002@gmail.com>
2020-06-14 00:41:02 +0900

    Add Time limit

--- Assets/GameScripts/PointManager.cs
+++ Assets/GameScripts/PointManager.cs
@@ -14,7 +14,12 @@ public class PointManager : MonoBehaviour
     public static bool defeat = false;
     Image img;
     Slider slider;
+    Text timelimit;
     int prevBites;
+
+    public static float gameStartTime;
+    float timeLimit = 30;
+    FirstTimeClass timerFt = new FirstTimeClass();
     void Start()
     {
         duration = 10;
@@ -24,18 +29,26 @@ public class PointManager : MonoBehaviour
         slider = GameObject.Find("Canvas/Slider").GetComponent<Slider>();
         img = GameObject.Find("Canvas/Slider/FillArea/Fill").GetComponent<Image>();
         img.color = new Color(0, 255, 0);
-
+        timelimit = GameObject.Find("Canvas/TimeLimit").GetComponent<Text>();
     }
 
     void Update()
     {
+        if (MapController.systemStatus != MapController.SystemStatus.GameStarted) {
+            timelimit.text = "Time limit:" + (timeLimit).ToString("F1");
+            return;
+        }
+        if(timerFt) {
+            gameStartTime = Time.timeSinceLevelLoad;
+        }
+        timelimit.text = "Time limit:" + (timeLimit - Time.timeSinceLevelLoad + gameStartTime).ToString("F1");
         if(hasPower) {
             diff = Time.timeSinceLevelLoad - startTime;
             if(diff >= duration) {
                 hasPower = false;
             }
         }        
-        if(numOfBite <= baites) { // パックマンが餌を食べ尽くした
+        if(numOfBite <= baites || Time.timeSinceLevelLoad - gameStartTime >= timeLimit) { // パックマンが餌を食べ尽くした
             // ゲームオーバー処理
             defeat = true;
         }
@@ -46,9 +59,8 @@ public class PointManager : MonoBehaviour
             // r -> value 1～0.5 の間0から255まで上がる
             // g -> value 0.5～0 の間255から0まで下がる
             img.color = new Color(slider.value >= 0.5 ? (float)(1-(slider.value-0.5) * 2) : 1, slider.value < 0.5 ? (float)(slider.value * 2) : 1, 0);
+            timelimit.color = new Color(1, img.color.g, 0);
         }
         prevBites = baites;
-
-        
     }
 }
